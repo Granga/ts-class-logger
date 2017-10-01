14f1210baeb9e880a117ed6fd217954f3453d630
Granga <temp.granga@gmail.com>
2017-10-01 02:07:23 +0200

    1.0.5

--- dist/logger.js
+++ dist/logger.js
@@ -1,42 +1,70 @@
 "use strict";
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const level_enum_1 = require("./level.enum");
-class Logger {
-    constructor(name, options) {
+var level_enum_1 = require("./level.enum");
+var Logger = /** @class */ (function () {
+    function Logger(name, options) {
         this.name = name;
-        this.options = Object.assign({ timestamp: false }, options);
+        this.options = __assign({ timestamp: false }, options);
     }
-    debug(...args) {
+    Logger.prototype.debug = function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
         return this._log(level_enum_1.Level.DEBUG, args);
-    }
-    log(...args) {
+    };
+    Logger.prototype.log = function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
         return this._log(level_enum_1.Level.LOG, args);
-    }
-    info(...args) {
+    };
+    Logger.prototype.info = function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
         return this.log(args);
-    }
-    warn(...args) {
+    };
+    Logger.prototype.warn = function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
         return this._log(level_enum_1.Level.WARN, args);
-    }
-    error(...args) {
+    };
+    Logger.prototype.error = function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
         return this._log(level_enum_1.Level.ERROR, args);
-    }
-    nameToString() {
+    };
+    Logger.prototype.nameToString = function () {
         if (typeof this.name == "string") {
             return this.name;
         }
         else {
             return "";
         }
-    }
-    _log(level, args) {
+    };
+    Logger.prototype._log = function (level, args) {
         try {
             if (this.options.level == level_enum_1.Level.OFF) {
-                return () => {
+                return function () {
                 };
             }
             if (this.rank(level) < this.rank(this.options.level)) {
-                return () => {
+                return function () {
                 };
             }
             switch (level) {
@@ -57,13 +85,14 @@ class Logger {
                     break;
             }
             (typeof this.options.all == "function") && this.options.all(level, args);
-            return console[level].bind(window.console, ...[`${this.options.timestamp ? new Date().toJSON() : ""}`, `[${this.nameToString()}]`, ...args]);
+            return (_a = console[level]).bind.apply(_a, [window.console].concat(["" + (this.options.timestamp ? new Date().toJSON() : ""), "[" + this.nameToString() + "]"].concat(args)));
         }
         catch (err) {
             console.error(err);
         }
-    }
-    rank(level) {
+        var _a;
+    };
+    Logger.prototype.rank = function (level) {
         switch (level) {
             case level_enum_1.Level.OFF:
                 return 255;
@@ -80,6 +109,7 @@ class Logger {
             default:
                 return 0;
         }
-    }
-}
+    };
+    return Logger;
+}());
 exports.Logger = Logger;
